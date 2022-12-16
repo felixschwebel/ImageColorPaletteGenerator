c003bf64c0aac7a17b524bf9eb91243cd66d766e
felixschwebel <schwebel.felix@gmail.com>
2022-12-16 07:04:54 +0100

    main elements

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+import os
 
 app = Flask(__name__)
+bootstrap = Bootstrap5()
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 
 @app.route("/")
