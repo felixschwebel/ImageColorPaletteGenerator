411e1a63c850dbfc54d34177e8e56307c84f7e69
felixschwebel <schwebel.felix@gmail.com>
2022-12-17 18:34:41 +0100

    buttons working

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
+from werkzeug.utils import secure_filename
 import os
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 bootstrap = Bootstrap5()
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-file_path = "static/img/example_colors/"
 example_colors = [['1878A8.png', '181848.png'], ['186090.png', '604848.png'], ['607830.png', '781818.png'],
                   ['F06018.png', 'F09030.png'], ['FF9018.png', 'FFA830.png']]
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    colors = example_colors
-    return render_template('index.html', colors=colors)
+    if request.method == 'POST':
+        print(request.files['file'])
+    return render_template('index.html', colors=example_colors)
 
 
 if __name__ == '__main__':
