90ba7dd31201fd753fcf448c3a399313a7b379cd
felixschwebel <schwebel.felix@gmail.com>
2022-12-19 09:10:37 +0100

    upload function working

--- main.py
+++ main.py
@@ -15,10 +15,23 @@ example_colors = [['1878A8.png', '181848.png'], ['186090.png', '604848.png'], ['
                   ['F06018.png', 'F09030.png'], ['FF9018.png', 'FFA830.png']]
 
 
+# check the file extensions
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        print(request.files['file'])
+        if 'file' not in request.files:
+            flash('No file part!')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file!')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return render_template('index.html', colors=example_colors)
 
 
