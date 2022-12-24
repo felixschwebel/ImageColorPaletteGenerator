21317a42b140cd09869ac47fd582e434146c04f4
felixschwebel <schwebel.felix@gmail.com>
2022-12-24 16:46:13 +0100

    functions working

--- check_colors.py
+++ check_colors.py
@@ -1,7 +1,6 @@
 import numpy as np
 from PIL import Image
-import pandas as pd
-import matplotlib.pyplot as plt
+
 
 test_file = 'static/img/steve-johnson-placeholder.jpg'
 test_file2 = 'static/uploads/coffee.jpg'
@@ -16,14 +15,13 @@ def check_colors(file):
     shape = img.shape
     # flatten the image
     shape_new = img.reshape(shape[0]*shape[1], shape[2])
-    df = pd.DataFrame()
-    hex_series = pd.Series([convert_to_hex(rgb) for rgb in shape_new])
-    df['colors'] = hex_series
-    df.value_counts()
-    #hex_df.hist(column=hex_df, bins=10)
-    #plt.show()
+    # use numpy histogram to sort the colors into 10 buckets
+    indices = np.histogram(shape_new, bins=10)[0]
+    # get rbg colors from array
+    rgb_colors = [shape_new[color] for color in indices]
+    # convert to hex
+    hex_colors = [convert_to_hex(value) for value in rgb_colors]
 
-    hex_colors = [color[0] for color in hex_df.index]
     colors = []
     start = 0
     end = 2
@@ -33,6 +31,3 @@ def check_colors(file):
         end += 2
 
     return colors
-
-
-check_colors(test_file2)
